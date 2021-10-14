ae8363d66ec52a2061bfba69ccbf38dc9b21f4a9
Johnny <johnnyfran20002@gmail.com>
2021-10-14 15:15:27 -0400

    created feed page

--- client/src/views/Feed.vue
+++ client/src/views/Feed.vue
@@ -1,13 +1,29 @@
 <template>
   <div class="section">
       <h1 class="title">
-          Feed Page
-      </h1>
+          Feed Page</h1>
+
+      <div class="columns">
+        <div class="column is-one-third is-offset-one-third">
+          <div class="post" v-for="p in posts" :key="p.src">
+            <post :post="p" />
+
+          </div>
+        </div>
+      </div>
   </div>
 </template>
 
 <script>
+import session from '../services/session'
+import { GetWall } from '../services/posts'
+import Post from '../components/Post.vue'
+
 export default {
+  components: { Post },
+  data: ()=> ({
+    posts: GetWall(session.user.handle)
+  })
 
 }
 </script>
