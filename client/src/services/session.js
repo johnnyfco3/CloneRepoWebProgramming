8631b3630c3c55cf1dd281e3671094cb08c4c332
Johnny <johnnyfran20002@gmail.com>
2021-10-18 15:17:40 -0400

    notification addition

--- client/src/services/session.js
+++ client/src/services/session.js
@@ -1,5 +1,6 @@
 import router from '../router'
 import { Login } from './users';
+import { NotificationProgrammatic } from "@oruga-ui/oruga-next/dist/esm/notification";
 
 const session = {
     user: null,
@@ -14,6 +15,13 @@ const session = {
             router.push(this.toRoute);
         } catch(error){
             this.messages.push({text: error.msg, type: 'warning'})
+            NotificationProgrammatic.open({
+                message: error.msg,
+                variant: 'danger',
+                type: 'danger',
+                closable: true,
+                duration: 5000
+            })
         }
     }
 };
