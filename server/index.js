5d05f71176b6eba9812c0479a00e6e3a8e3334b1
Johnny <johnnyfran20002@gmail.com>
2021-10-21 15:12:22 -0400

    built web server

--- server/index.js
+++ server/index.js
@@ -1,20 +1,18 @@
 const express = require('express')
+const path = require('path')
+
 const usersController = require('./controllers/users')
+
 const app = express()
 const port = 3000
 
 app
-  .get('*', (req, res, next)=>{
-    console.log('A request came in');
-    next();
-  })
-  .get('/', (req, res) => {
-    res.send('Hello World!')
-  })
-  .get('/newpaltz', (req, res) =>{
-    res.send('Hello New Paltz!')
-  })
+  .use('/', express.static(path.join(__dirname, '../docs')))
   .use('/users', usersController)
+
+app
+  .get('*', (req, res)=>
+    res.sendFile(path.join(__dirname, '../docs/index.html')))
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
