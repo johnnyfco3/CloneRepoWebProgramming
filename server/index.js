afec471ab3a2b29b4ccc61ac65bf2282da245317
Johnny <johnnyfran20002@gmail.com>
2021-10-21 14:54:04 -0400

    working API

--- server/index.js
+++ server/index.js
@@ -1,14 +1,20 @@
 const express = require('express')
+const usersController = require('./controllers/users')
 const app = express()
 const port = 3000
 
 app
-.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-.get('/newpaltz', (req, res) =>{
-  res.send('Hello New Paltz!')
-})
+  .get('*', (req, res, next)=>{
+    console.log('A request came in');
+    next();
+  })
+  .get('/', (req, res) => {
+    res.send('Hello World!')
+  })
+  .get('/newpaltz', (req, res) =>{
+    res.send('Hello New Paltz!')
+  })
+  .use('/users', usersController)
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
